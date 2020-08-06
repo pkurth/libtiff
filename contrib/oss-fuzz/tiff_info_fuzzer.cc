ae9b5c71d68c594478d9e790815d6dca22143b96
Ravi Jotwani <rjotwani@google.com>
2020-08-05 17:42:01 -0700

    pass istream pointer instead of object

--- contrib/oss-fuzz/tiff_info_fuzzer.cc
+++ contrib/oss-fuzz/tiff_info_fuzzer.cc
@@ -15,7 +15,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
-#include <istream>
+#include <sstream>
 #include <iostream>
 
 #include <fcntl.h>
@@ -291,10 +291,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
   const std::string s(reinterpret_cast<const char*>(data), size);
   std::istringstream iss(s);
-  std::istream& stream = iss;
+  std::istream stream = iss;
 
   TiffStream ts;
-  TIFF *tif = ts.makeFileStream(stream);
+  TIFF *tif = ts.makeFileStream(&stream);
   if (tif == nullptr)
     return 0;
 
