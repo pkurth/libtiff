34292d2f5160f97f9d1a6c9457a65f69dea20c40
Ravi Jotwani <rjotwani@google.com>
2020-08-06 16:39:58 -0700

    build extra_dist

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -54,7 +54,7 @@ if [ "$ARCHITECTURE" != "i386" ]; then
     apt-get install -y liblzma-dev
 fi
 
-cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DBUILD_SHARED_LIBS=off
+cmake . -DCMAKE_INSTALL_PREFIX=$WORK -DBUILD_SHARED_LIBS=off -DEXTRA_DIST=on
 make -j$(nproc)
 make install
 
