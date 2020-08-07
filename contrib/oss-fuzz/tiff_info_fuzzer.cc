b642ac4264092e36e04eca731a7d454191be9ad6
Ravi Jotwani <rjotwani@google.com>
2020-08-07 10:21:28 -0700

    removed instances of printf

--- contrib/oss-fuzz/tiff_info_fuzzer.cc
+++ contrib/oss-fuzz/tiff_info_fuzzer.cc
@@ -32,22 +32,6 @@ constexpr size_t kMaxMalloc = 250000000; // 1/4 GB
 
 static bool showdata = false;  // show data
 static bool rawdata = false;   // show raw/decoded data
-static bool showwords = false; // show data as bytes/words
-static bool readdata = true;   // read data in file
-static bool stoponerr = true;  // stop on first read error
-
-static void ShowStrip(tstrip_t strip, unsigned char *pp, uint32 nrow,
-                      tsize_t scanline) {
-  printf("Strip %lu:\n", (unsigned long)strip);
-  while (nrow-- > 0) {
-    for (tsize_t cc = 0; cc < scanline; cc++) {
-      printf(" %02x", *pp++);
-      if (((cc + 1) % 24) == 0)
-        putchar('\n');
-    }
-    putchar('\n');
-  }
-}
 
 void TIFFReadContigStripData(TIFF *tif) {
   const tsize_t scanline = TIFFScanlineSize(tif);
@@ -67,10 +51,7 @@ void TIFFReadContigStripData(TIFF *tif) {
     const uint32 nrow = (row + rowsperstrip > h ? h - row : rowsperstrip);
     tstrip_t strip = TIFFComputeStrip(tif, row, 0);
     if (TIFFReadEncodedStrip(tif, strip, buf, nrow * scanline) < 0) {
-      if (stoponerr)
-        break;
-    } else if (showdata) {
-      ShowStrip(strip, buf, nrow, scanline);
+      break;
     }
   }
   _TIFFfree(buf);
@@ -96,30 +77,11 @@ void TIFFReadSeparateStripData(TIFF *tif) {
       uint32 nrow = (row + rowsperstrip > h ? h - row : rowsperstrip);
       tstrip_t strip = TIFFComputeStrip(tif, row, s);
       if (TIFFReadEncodedStrip(tif, strip, buf, nrow * scanline) < 0) {
-        if (stoponerr)
-          break;
-      } else if (showdata) {
-        ShowStrip(strip, buf, nrow, scanline);
+        break;
       }
     }
   }
   _TIFFfree(buf);
-}
-
-static void ShowTile(uint32 row, uint32 col, tsample_t sample,
-                     unsigned char *pp, uint32 nrow, tsize_t rowsize) {
-  printf("Tile (%lu,%lu", (unsigned long)row, (unsigned long)col);
-  if (sample != (tsample_t)-1)
-    printf(",%u", sample);
-  printf("):\n");
-  while (nrow-- > 0) {
-    for (uint32 cc = 0; cc < (uint32)rowsize; cc++) {
-      printf(" %02x", *pp++);
-      if (((cc + 1) % 24) == 0)
-        putchar('\n');
-    }
-    putchar('\n');
-  }
 }
 
 void TIFFReadContigTileData(TIFF *tif) {
@@ -139,10 +101,7 @@ void TIFFReadContigTileData(TIFF *tif) {
   for (uint32 row = 0; row < h; row += th) {
     for (uint32 col = 0; col < w; col += tw) {
       if (TIFFReadTile(tif, buf, col, row, 0, 0) < 0) {
-        if (stoponerr)
-          break;
-      } else if (showdata) {
-        ShowTile(row, col, (tsample_t)-1, buf, th, rowsize);
+        break;
       }
     }
   }
@@ -169,10 +128,7 @@ void TIFFReadSeparateTileData(TIFF *tif) {
     for (uint32 col = 0; col < w; col += tw) {
       for (s = 0; s < samplesperpixel; s++) {
         if (TIFFReadTile(tif, buf, col, row, 0, s) < 0) {
-          if (stoponerr)
-            break;
-        } else if (showdata) {
-          ShowTile(row, col, s, buf, th, rowsize);
+          break;
         }
       }
     }
@@ -197,27 +153,9 @@ void TIFFReadData(TIFF *tif) {
   }
 }
 
-static void ShowRawBytes(unsigned char *pp, uint32 n) {
-  for (uint32 i = 0; i < n; i++) {
-    printf(" %02x", *pp++);
-    if (((i + 1) % 24) == 0)
-      printf("\n ");
-  }
-  putchar('\n');
-}
-
-static void ShowRawWords(uint16 *pp, uint32 n) {
-  for (uint32 i = 0; i < n; i++) {
-    printf(" %04x", *pp++);
-    if (((i + 1) % 15) == 0)
-      printf("\n ");
-  }
-  putchar('\n');
-}
-
 void TIFFReadRawData(TIFF *tif, int bitrev) {
   const tstrip_t nstrips = TIFFNumberOfStrips(tif);
-  const char *what = TIFFIsTiled(tif) ? "Tile" : "Strip";
+  TIFFIsTiled(tif);
   uint64 *stripbc = nullptr;
 
   TIFFGetField(tif, TIFFTAG_STRIPBYTECOUNTS, &stripbc);
@@ -236,25 +174,14 @@ void TIFFReadRawData(TIFF *tif, int bitrev) {
       bufsize = (uint32)stripbc[s];
     }
     if (buf == nullptr) {
-      fprintf(stderr, "Cannot allocate buffer to read strip %lu\n",
-              (unsigned long)s);
       break;
     }
     if (TIFFReadRawStrip(tif, s, buf, (tmsize_t)stripbc[s]) < 0) {
-      fprintf(stderr, "Error reading strip %lu\n", (unsigned long)s);
-      if (stoponerr)
-        break;
+      break;
     } else if (showdata) {
       if (bitrev) {
         TIFFReverseBits(reinterpret_cast<uint8 *>(buf), (tmsize_t)stripbc[s]);
-        printf("%s %lu: (bit reversed)\n ", what, (unsigned long)s);
-      } else {
-        printf("%s %lu:\n ", what, (unsigned long)s);
       }
-      if (showwords)
-        ShowRawWords((uint16 *)buf, (uint32)stripbc[s] >> 1);
-      else
-        ShowRawBytes((unsigned char *)buf, (uint32)stripbc[s]);
     }
   }
   if (buf != nullptr)
@@ -263,7 +190,7 @@ void TIFFReadRawData(TIFF *tif, int bitrev) {
 
 static void tiffinfo(TIFF *tif, uint16 order, long flags, int is_image) {
   TIFFPrintDirectory(tif, stdout, flags);
-  if (!readdata || !is_image)
+  if (!is_image)
     return;
   if (rawdata) {
     if (order) {
